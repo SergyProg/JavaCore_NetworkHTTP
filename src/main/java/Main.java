aeb60aa3452effab9132923e6da2664ff63db095
SergyProg <serj.shoping@yandex.ru>
2022-03-27 16:48:13 +0300

    Запрос на получение списка фактов о кошках. Вер.1

---
+++ src/main/java/Main.java
@@ -0,0 +1,45 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static final String REMOTE_SERVICE_URI = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+    public static final ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setUserAgent("My Test Service")
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)   // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .build())
+                .build();
+
+        HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+
+        CloseableHttpResponse response = httpClient.execute(request);
+        //Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
+
+        List<Fact> posts = mapper.readValue(response.getEntity().getContent(), new TypeReference<List<Fact>>() {});
+        posts.stream()
+                .filter(value ->  value.getUpvotes() != null) // в задании сказано отсечь только upvotes == null
+                .forEach(value -> System.out.println(value));
+        //posts.forEach(System.out::println);
+    }
+}
+
+
+
+
